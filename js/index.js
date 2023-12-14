23a711228210ce18395ffedf0b7fb7113f51a157
Anton <137609245+Ddonec@users.noreply.github.com>
2023-12-14 16:09:54 +0300

    feat: catalog filter MVP

--- js/index.js
+++ js/index.js
@@ -59,3 +59,41 @@ checkbox.addEventListener("change", function () {
       numberHeader.style.color = "#5567EA";
    }
 });
+
+// Фильтр в каталоге
+
+const priceSlider = document.getElementById("price-slider");
+const priceMin = document.getElementById("price-min");
+const priceMax = document.getElementById("price-max");
+const inputMin = document.getElementById("input-min");
+const inputMax = document.getElementById("input-max");
+
+noUiSlider.create(priceSlider, {
+   start: [300, 50000],
+   connect: true,
+   range: {
+      min: 300,
+      max: 50000,
+   },
+   handles: true, // Добавлено для отображения кружочков
+});
+
+priceSlider.noUiSlider.on("update", function (values, handle) {
+   if (handle === 0) {
+      priceMin.textContent = "от " + Math.round(values[0]);
+      inputMin.value = Math.round(values[0]);
+   }
+   if (handle === 1) {
+      priceMax.textContent = "до " + Math.round(values[1]);
+      inputMax.value = Math.round(values[1]);
+   }
+});
+
+// Обновление значений ползунков при вводе в поля
+inputMin.addEventListener("change", function () {
+   priceSlider.noUiSlider.set([this.value, null]);
+});
+
+inputMax.addEventListener("change", function () {
+   priceSlider.noUiSlider.set([null, this.value]);
+});
