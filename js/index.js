688a2ccddb88e9c5d486bc1f1ada3d66cc013545
Anton <137609245+Ddonec@users.noreply.github.com>
2023-12-19 23:11:25 +0300

    feat: switch summer & winter mode road(no complet)

--- js/index.js
+++ js/index.js
@@ -7,9 +7,28 @@ const secondLineHeader = document.querySelector(".second-line-header");
 const headerList = document.querySelector(".header-1920-list");
 const header2List = document.querySelector(".icun-list-h");
 const searchZoneHeader = document.getElementById("search-header-1920");
+const searchZoneHeaderClass = document.querySelector(".search-header-1920");
 const summerStatus = document.querySelectorAll(".summer-status");
 const winterStatus = document.querySelectorAll(".winter-status");
 const numberHeader = document.querySelector(".n-h-y");
+const textGradient = document.querySelectorAll(".text-gradient");
+const backgroundHeader = document.querySelector(".bacground-image-main-page-first-section");
+const backgroundHeaderLeftPhoto = document.querySelector(".absolute-fs-container-left");
+const backgroundHeaderRightPhoto = document.querySelector(".absolute-fs-container-right");
+const switchSeasonBtns = document.querySelectorAll(".class-to-switch-season-btn");
+const searchIconSwitchW = document.getElementById("search-icon-1920");
+const searchIconSwitchB = document.getElementById("search-icon-1920-black");
+const burgerMenuSwitchColor = document.querySelectorAll(".burger span");
+const bgSixSection = document.querySelector(".six-section");
+const switchTextColorSixS = document.querySelectorAll(".switch-color-text-black-white");
+const textTitlePartBlue = document.querySelectorAll(".text-blocks-6-s div:nth-child(1) > span");
+const runStringFirst = document.querySelector(".roll-park-list");
+const runStringSecond = document.querySelector(".roll-park-list2");
+const yellowBlock = document.querySelector(".yellow-block");
+const borderColorSwitch = document.querySelectorAll(".border-switch-status-blue");
+const sixSectionTitle = document.querySelector(".title-of-section-gradient-yellow");
+const fourSectionContainerHide = document.querySelector(".four-section");
+const fiveSectionContainerHide = document.querySelector(".five-section");
 
 const blueFake = document.querySelector(".blue-line-fake");
 const grayFake = document.querySelector(".gray-line-fake");
@@ -21,8 +40,26 @@ checkbox.addEventListener("change", function () {
       summerStatus.forEach(function (element) {
          element.classList.add("none");
       });
+      textGradient.forEach(function (element) {
+         element.classList.remove("text-gradient-summer");
+      });
       winterStatus.forEach(function (element) {
          element.classList.remove("none");
+      });
+      switchSeasonBtns.forEach(function (element) {
+         element.classList.remove("blue-b-g");
+      });
+      burgerMenuSwitchColor.forEach(function (element) {
+         element.style.background = "white";
+      });
+      switchTextColorSixS.forEach(function (element) {
+         element.classList.remove("switch-color-text-black");
+      });
+      textTitlePartBlue.forEach(function (element) {
+         element.style.color = "#ffc93a";
+      });
+      borderColorSwitch.forEach(function (element) {
+         element.classList.remove("border-blue");
       });
       summerContent.style.display = "none";
       winterContent.style.display = "flex";
@@ -31,18 +68,51 @@ checkbox.addEventListener("change", function () {
       secondLineHeader.style.background = "rgba(232, 234, 251, 0.8)";
       headerList.style.color = "white";
       header2List.style.color = "white";
+      searchZoneHeaderClass.classList.remove("custom-placeholder-style");
       searchZoneHeader.style.border = "2px solid #fff";
       searchZoneHeader.style.background = "#5567ea";
       placeholderElement.style.color = newPlaceholderColor;
       blueFake.style.background = "#5567ea";
       grayFake.style.background = "rgba(232, 234, 251, 0.8)";
       numberHeader.style.color = "#FFC93A";
+      backgroundHeaderRightPhoto.style.backgroundImage = "url(../assets/content/right-snow-peoples.png)";
+      backgroundHeaderLeftPhoto.style.backgroundImage = "url(../assets/content/left-snow-peoples-3.png)";
+      backgroundHeader.style.backgroundImage = "url(../assets/content/header-bg-without-noize.png)";
+      searchIconSwitchB.classList.add("none");
+      searchIconSwitchW.classList.remove("none");
+      bgSixSection.style.backgroundImage = "url(../assets/content/Noise&Texture.png)";
+      bgSixSection.style.backgroundColor = "#5567ea";
+
+      runStringFirst.classList.remove("roll-park-list-black");
+      runStringSecond.classList.remove("roll-park-list-black");
+      yellowBlock.style.background = "#ffc93a";
+      sixSectionTitle.classList.remove("text-gradient");
+      fourSectionContainerHide.classList.add("none");
+      fiveSectionContainerHide.classList.add("none");
    } else {
       summerStatus.forEach(function (element) {
          element.classList.remove("none");
       });
+      textGradient.forEach(function (element) {
+         element.classList.add("text-gradient-summer");
+      });
       winterStatus.forEach(function (element) {
          element.classList.add("none");
+      });
+      switchSeasonBtns.forEach(function (element) {
+         element.classList.add("blue-b-g");
+      });
+      burgerMenuSwitchColor.forEach(function (element) {
+         element.style.background = "black";
+      });
+      switchTextColorSixS.forEach(function (element) {
+         element.classList.add("switch-color-text-black");
+      });
+      textTitlePartBlue.forEach(function (element) {
+         element.style.color = "#5567EA";
+      });
+      borderColorSwitch.forEach(function (element) {
+         element.classList.add("border-blue");
       });
       winterContent.style.display = "none";
       summerContent.style.display = "flex";
@@ -51,12 +121,29 @@ checkbox.addEventListener("change", function () {
       secondLineHeader.style.background = "#fff9e8";
       headerList.style.color = "black";
       header2List.style.color = "black";
+      searchZoneHeaderClass.classList.add("custom-placeholder-style");
+
       searchZoneHeader.style.border = "2px solid black";
       searchZoneHeader.style.background = "#ffdd64";
+      searchZoneHeader.style.color = "black";
       placeholderElement.style.color = "black";
       blueFake.style.background = "#ffc93a";
       grayFake.style.background = "#fff9e8";
       numberHeader.style.color = "#5567EA";
+      backgroundHeaderRightPhoto.style.backgroundImage = "url(../assets/content/samokat-rider-summer.png)";
+      backgroundHeaderLeftPhoto.style.backgroundImage = "url(../assets/content/bike-riders-summer.png)";
+      backgroundHeader.style.backgroundImage = "url(../assets/content/head-bg-summer.png)";
+      searchIconSwitchB.classList.remove("none");
+      searchIconSwitchW.classList.add("none");
+      bgSixSection.style.backgroundImage = "url(../assets/content/bg-6-section-summer.png)";
+      bgSixSection.style.backgroundColor = "unset";
+
+      runStringFirst.classList.add("roll-park-list-black");
+      runStringSecond.classList.add("roll-park-list-black");
+      yellowBlock.style.background = "white";
+      sixSectionTitle.classList.add("text-gradient");
+      fourSectionContainerHide.classList.remove("none");
+      fiveSectionContainerHide.classList.remove("none");
    }
 });
 
